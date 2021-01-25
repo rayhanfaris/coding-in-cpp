6afad96c50309af2b50d4ee6ea835987652f8a5b
rayhanfaris <rayhanjufahri11@gmail.com>
2021-01-25 21:12:04 +0700

    Add functionality in .cpp

--- design-pattern-cpp/singleton.cpp
+++ design-pattern-cpp/singleton.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "singleton.h"
+
 using namespace std;
 
-int main(){
+Singleton *Singleton::instance = nullptr; //or nullptr
 
+int main(){
+    Singleton* s = s->getInstance();
+    cout << s->getData() << endl;
+    s->setData(50);
+    cout << s->getData() << endl;
     return 0;
 }
